d64a74784f521ddb758b8b94ab869816d83e3b94
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2026-06-21 22:56:22 -0600

    Add loader.py file

---
+++ modules/loader.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from typing import Iterator
+from .config import DenueConfig
+import logging
+
+logger = loggin.getLogget(__name__)
+
+class DenueDataLoader:
+
+    def __init__(self, config: DenueConfig):
+        self.config = config
+
+    def load(self) -> Iterator[pd.DataFrame]:
+        logger.info(f"Loading File: {self.config.input_file}")
+        if self.config.chunk_size:
+            chunks = pd.read_csv(
+                self.config.input_file,
+                encoding=self.config.encoding,
+                chunksize=self.config.chunk_size,
+                load_memory=False
+            )
+            for chunk in chunks:
+                yield chunk
+        else:
+            df = pd.read_csv(
+                self.config.input_file,
+                encoding=self.config.encoding,
+                low_memory=False
+            )
+            yield df
